2c43dae7f9f86d074e7b11a86238e468bced94ec
Xin Wei Chow <chowxinwei@gmail.com>
2018-01-04 23:46:11 +1100

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,68 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+
+  private Crime crime;
+  private EditText titleField;
+  private Button dateButton;
+  private CheckBox solvedCheckBox;
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    crime = new Crime();
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+    titleField = (EditText) v.findViewById(R.id.crime_title);
+    titleField.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        // This space intentionally left blank
+      }
+
+      @Override
+      public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        crime.setTitle(charSequence.toString());
+      }
+
+      @Override
+      public void afterTextChanged(Editable editable) {
+        // This space intentionally left blank
+      }
+    });
+
+    dateButton = (Button) v.findViewById(R.id.crime_date);
+    dateButton.setText(crime.getDate().toString());
+    dateButton.setEnabled(false);
+
+    solvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+    solvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        crime.setSolved(b);
+      }
+    });
+
+    return v;
+  }
+}
