93166cee0757e016b2fe633f9ebb770f35d2cf32
Xin Wei Chow <chowxinwei@gmail.com>
2018-01-05 20:52:07 +1100

    Add CrimeListFragment

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,43 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+  private static CrimeLab crimeLab;
+
+  private List<Crime> crimes;
+
+  public static CrimeLab get(Context context) {
+    if (crimeLab == null) {
+      crimeLab = new CrimeLab(context);
+    }
+    return crimeLab;
+  }
+
+  private CrimeLab(Context context) {
+    crimes = new ArrayList<>();
+    for (int i = 0; i < 100; i++) {
+      Crime crime = new Crime();
+      crime.setTitle("Crime #" + i);
+      crime.setSolved(i % 2 == 0);
+      crimes.add(crime);
+    }
+  }
+
+  public List<Crime> getCrimes() {
+    return crimes;
+  }
+
+  public Crime getCrime(UUID id) {
+    for (Crime crime : crimes) {
+      if (crime.getId() == id) {
+        return crime;
+      }
+    }
+
+    return null;
+  }
+}
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CrimeListFragment extends Fragment {
+
+  private RecyclerView crimeRecyclerView;
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+    crimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+    crimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+    return view;
+  }
+}
